c6483c583b831b89f9ed714b3fc51d88bee55293
JorySchiebroek <joryschiebroek@gmail.com>
2019-10-27 15:35:03 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  language: string;
+  locales = ['en', 'da', 'nl'];
   title = 'schiebroekjs';
+  year = new Date().getFullYear();
+
+  ngOnInit() {
+    this.changeLanguage(navigator.language.substring(0, 2));
+  }
+
+  changeLanguage(language) {
+    if (this.locales.includes(language)) {
+      this.language = language;
+    } else {
+      this.language = 'en';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {
+  faEnvelope
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faDribbble,
+  faGithub,
+  faLinkedin
+} from '@fortawesome/free-brands-svg-icons';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +21,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faDribbble,
+      faEnvelope,
+      faGithub,
+      faLinkedin
+    );
+  }
+}
